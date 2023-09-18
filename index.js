3eeaed9f12e75a600f82de784c33bd1e636abb45
Aloysius Kang <aloysius.kang1103@gmail.com>
2023-09-18 13:44:55 +0700

    Created Home Page

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const path = require("path");
 const express = require("express");
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Testing website");
+})
+
+app.listen(PORT, () => console.log(`Server launched at port ${PORT}`));
+
 
